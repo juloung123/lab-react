e6b92032440b2f4bfa37912ee243d7395dde5329
juloung123 <jokergamer123@hotmail.com>
2020-08-16 00:03:58 +0700

    third commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
+const word = "Hello";
 function App() {
   return (
     <div>
-      Hello world!!
+      <WordCard value={word}/>
     </div>
   );
 }
