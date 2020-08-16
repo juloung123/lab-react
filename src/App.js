1eaa40a744cce981c5d7b1cfd28b0834671e920e
juloung123 <jokergamer123@hotmail.com>
2020-08-16 20:47:05 +0700

    add random word

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './App.css';
 import CharacterCard from './CharacterCard';
 import WordCard from './WordCard';
-const word = "Hello";
+
+let allword = ['Apple','banana','hello','react','javascript','yarn']
+
+const word = allword[Math.floor(Math.random() * 6)];
+
 function App() {
   return (
     <div>
