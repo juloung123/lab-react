fbbf26d11ec4a50043b816ae217c5c1e4bad2e70
juloung123 <jokergamer123@hotmail.com>
2020-08-29 21:55:38 +0700

    add help botton but console

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import WordCard from './WordCard';
 let allword = ['Apple','banana','hello','react','javascript','yarn']
 
 function App() {
+  const midset = 'midset'
   const className1 = 'reloadbotton'
   function refreshPage() {
     window.location.reload(false);
   }
   
   return (
-    <div>
+    <div style={styles.style}>
       <WordCard value={allword}/>
       <button onClick={refreshPage} className = {className1}>Click to reload!</button>
     </div>
@@ -20,3 +21,12 @@ function App() {
 }
 
 export default App;
+
+const styles = ({
+  style : {
+      display: "flex",
+      flexDirection:'column',
+      justifyContent: "center",
+      alignItems: "center",
+    }
+})
--- src/WordCard.js
+++ src/WordCard.js
@@ -10,19 +10,28 @@ const prepareStateFromword = given_word => {
         chars,
         attempt: 1,
         guess: '',
-        complete: false
+        complete: false,
+        help:1,
+        check:0,
     }
 }
 
 export default function WordCard(props){
+    const className1 = 'reloadbotton'
     const word1 = props.value[Math.floor(Math.random() * 6)];
     const [state, setState] = useState(prepareStateFromword(word1))
-
+    let sethelp = state.help-1
+    // let helpguess = state.guess + state.word.charAt(state.check)
+    function help(){ 
+        console.log(`${state.word.charAt(state.check)} has been activated`)
+    }
     const activationHandler = c => {
         console.log(`${c} has been activated`)
-
+        let check = state.check + 1
         let guess = state.guess + c
-        setState({...state, guess})
+        
+        setState({...state, guess , check})
+        console.log(`${state.check} has been activated`)
 
         if(guess.length ==  state.word.length){
             if(guess == state.word){
@@ -31,7 +40,7 @@ export default function WordCard(props){
             }
             else{
                 console.log('reset, next attempt')
-                setState({...state, guess : '' , attempt : state.attempt + 1})
+                setState({...state, guess : '' , attempt : state.attempt + 1 , check : 0})
                 console.log(state.attempt)
             }
         }
@@ -44,6 +53,15 @@ export default function WordCard(props){
                     <CharacterCard value={c} key={i} activationHandler={activationHandler} attempt={state.attempt}/>
                 )
             }
+            <botton onClick = {help} className = {className1}>Help</botton>
         </div>
     )
 }
+const styles = ({
+    style : {
+        display: "flex",
+        flexDirection:'column',
+        justifyContent: "center",
+        alignItems: "center",
+      }
+  })
